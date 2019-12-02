45aaace578a860e70370188b44b887fca554f9f1
mob-sakai <sakai@mobcast.jp>
2019-12-02 19:14:27 +0900

    feat: Add basic option
    Close #3
    Close #4

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using CommandLine;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using System;
 using System.IO;
@@ -6,65 +7,77 @@ using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
 
-public class InternalAccessBridgeGenerator
+public class InternalAccessCompiler
 {
-	public static void Main (string [] args)
+	class Options
 	{
-		string inputCsProjPath = args [0];
-		string outputAsemblyPath = args [1];
-		string outputAsemblyName = Path.GetFileNameWithoutExtension (outputAsemblyPath);
+		[Option('o', "output", Required = false, HelpText = "Output path")]
+		public string Output { get; set; }
+		[Value(1, MetaName = "ProjectPath", HelpText = "Input .csproj path")]
+		public string ProjectPath { get; set; }
+	}
 
-		Console.WriteLine ($"Input Project File: {inputCsProjPath}");
-		Console.WriteLine ($"Output Asembly Path: {outputAsemblyPath}");
-		Console.WriteLine ($"Output Asembly Name: {outputAsemblyName}");
+	public static void Main(string[] args)
+	{
+		Parser.Default.ParseArguments<Options>(args)
+			.WithParsed(Compile);
+	}
 
-		var csproj = File.ReadAllLines (inputCsProjPath);
+	static void Compile(Options opt)
+	{
+		string inputCsProjPath = opt.ProjectPath;
+		string outputAsemblyPath = opt.Output;
+		string outputAsemblyName = Path.GetFileNameWithoutExtension(outputAsemblyPath);
+
+		Console.WriteLine($"Input Project File: {inputCsProjPath}");
+		Console.WriteLine($"Output Asembly Path: {outputAsemblyPath}");
+		Console.WriteLine($"Output Asembly Name: {outputAsemblyName}");
+
+		var csproj = File.ReadAllLines(inputCsProjPath);
 
 		// CSharpCompilationOptions
 		// MetadataImportOptions.All
-		var compilationOptions = new CSharpCompilationOptions (OutputKind.DynamicallyLinkedLibrary, allowUnsafe: true)
-			.WithMetadataImportOptions (MetadataImportOptions.All);
+		var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, allowUnsafe: true)
+			.WithMetadataImportOptions(MetadataImportOptions.All);
 
 		// BindingFlags.IgnoreAccessibility
-		typeof (CSharpCompilationOptions)
-			.GetProperty ("TopLevelBinderFlags", BindingFlags.Instance | BindingFlags.NonPublic)
-			.SetValue (compilationOptions, (uint)1 << 22);
-
+		typeof(CSharpCompilationOptions)
+			.GetProperty("TopLevelBinderFlags", BindingFlags.Instance | BindingFlags.NonPublic)
+			.SetValue(compilationOptions, (uint)1 << 22);
 
 		// Get all references.
-		var reg_dll = new Regex ("<HintPath>(.*)</HintPath>", RegexOptions.Compiled);
+		var reg_dll = new Regex("<HintPath>(.*)</HintPath>", RegexOptions.Compiled);
 		var metadataReferences = csproj
-			.Select (line => reg_dll.Match (line))
-			.Where (match => match.Success)
-			.Select (match => match.Groups [1].Value)
-			//.Concat (new [] { Assembly.Load (new AssemblyName ("System.Runtime")).Location })
-			.Select (path => MetadataReference.CreateFromFile (path));
+			.Select(line => reg_dll.Match(line))
+			.Where(match => match.Success)
+			.Select(match => match.Groups[1].Value)
+			.Select(path => MetadataReference.CreateFromFile(path));
 
 		// Get all preprocessor symbols.
-		var reg_preprocessorSymbols = new Regex ("<DefineConstants>(.*)</DefineConstants>", RegexOptions.Compiled);
+		var reg_preprocessorSymbols = new Regex("<DefineConstants>(.*)</DefineConstants>", RegexOptions.Compiled);
 		var preprocessorSymbols = csproj
-			.Select (line => reg_preprocessorSymbols.Match (line))
-			.Where (match => match.Success)
-			.SelectMany (match => match.Groups [1].Value.Split (';'));
+			.Select(line => reg_preprocessorSymbols.Match(line))
+			.Where(match => match.Success)
+			.SelectMany(match => match.Groups[1].Value.Split(';'));
 
 		// Get all source codes.
-		var parserOption = new CSharpParseOptions (LanguageVersion.CSharp7, preprocessorSymbols: preprocessorSymbols);
-		var reg_cs = new Regex ("<Compile Include=\"(.*\\.cs)\"", RegexOptions.Compiled);
+		var parserOption = new CSharpParseOptions(LanguageVersion.CSharp7, preprocessorSymbols: preprocessorSymbols);
+		var reg_cs = new Regex("<Compile Include=\"(.*\\.cs)\"", RegexOptions.Compiled);
 		var syntaxTrees = csproj
-			.Select (line => reg_cs.Match (line))
-			.Where (match => match.Success)
-			.Select (match => match.Groups [1].Value.Replace ('\\', Path.DirectorySeparatorChar))
-			.Select (path => CSharpSyntaxTree.ParseText (File.ReadAllText (path), parserOption, path));
+			.Select(line => reg_cs.Match(line))
+			.Where(match => match.Success)
+			.Select(match => match.Groups[1].Value.Replace('\\', Path.DirectorySeparatorChar))
+			.Select(path => CSharpSyntaxTree.ParseText(File.ReadAllText(path), parserOption, path));
 
 		// Start compiling.
-		var result = CSharpCompilation.Create (outputAsemblyName, syntaxTrees, metadataReferences, compilationOptions)
-			.Emit (outputAsemblyPath);
+		var result = CSharpCompilation.Create(outputAsemblyName, syntaxTrees, metadataReferences, compilationOptions)
+			.Emit(outputAsemblyPath);
 
 		// Output compile errors.
-		foreach (var d in result.Diagnostics.Where (d => d.IsWarningAsError || d.Severity == DiagnosticSeverity.Error))
+		foreach (var d in result.Diagnostics.Where(d => d.IsWarningAsError || d.Severity == DiagnosticSeverity.Error))
 		{
-			Console.WriteLine (string.Format ("{0} ({1}): {2} {3}", d.Severity, d.Id, d.GetMessage (), d.Location.GetMappedLineSpan ()));
+			Console.WriteLine(string.Format("{0} ({1}): {2} {3}", d.Severity, d.Id, d.GetMessage(), d.Location.GetMappedLineSpan()));
 		}
-		Console.WriteLine (result.Success ? "Success" : "Failed");
+		Console.WriteLine(result.Success ? "Success" : "Failed");
 	}
 }
